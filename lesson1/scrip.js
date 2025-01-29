aa403a2d63a161e327545ce7fe5db042a943aa70
Melikulov Komronbek <komronbekmelikulov0@gmail.com>
2025-01-29 15:32:16 +0800

    lesson 1

--- lesson1/scrip.js
+++ lesson1/scrip.js
@@ -1,1 +1,21 @@
-console.log('salom');
+// let d = 1;
+// let g = '12';
+// console.log(Number(g));
+//============
+// console.log((( 1 + 1) *1 ) /1);
+//============
+// let a = 12;
+// let b = 113;
+// console.log(a < b);
+
+// let s = 12;
+// let f = 12;
+// console.log(s >= f);
+// console.log(s <= f);
+
+// let s = 12;
+// let f = 12;
+// console.log(s == f);
+// console.log(s === f);
+
+
