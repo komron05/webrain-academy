850351a15e11c3f82b164a08dc7bb73f6c0d81d8
Melikulov Komronbek <komronbekmelikulov0@gmail.com>
2025-01-29 23:02:32 +0800

    if else and Nullish added

---
+++ lesson3/scrip.js
@@ -0,0 +1,50 @@
+//if else lesson
+
+// let name = "yomgir";
+
+// if (name == "yomgir"){
+//     console.log('hozir xavo yomgir');
+// } else if(name === 'qor'){
+// console.log('hozir xavo qor');
+// } else if (name == 'isiq'){
+//     console.log('havo isiq');
+// } else{
+//     console.log('men hazirgi pagodani bilmiman');
+// }
+//============================
+
+// let age = "dfghghg";
+// if (age >= 18){
+//     console.log('can open gmail');
+// }else if(age < 18 ){
+//     console.log("can't open gmail");
+// }else{
+//     console.log("just enter a number");
+// }
+//============================================
+// let temps = 3;
+// if (temps < 0 ){
+//     console.log('havo - darajada sovuq');
+// } else if(temps <= 10 && temps > 0){
+//     console.log('havo sovuq');
+// }else if (temps <= 15 && temps > 10){
+//     console.log('havo issiq');
+// }else if(temps <= 25 && temps > 15){
+//     console.log('havo juda issiq');
+// }else{
+//     console.log('not fund');
+// }
+
+//Nullish [ ?? ] -> null and undefined folse dep ketadi
+//OR [ || ] null, undefined, 0, false, NaN.
+
+// console.log(3 ?? 2);
+// console.log(2 ?? 3);
+// console.log(undefined ?? 3);
+// console.log(null ?? 3);
+
+// console.log(null || 2);
+// console.log(0 || 2);
+// console.log(false || 2); 
+// console.log(undefined || 2);  
+
