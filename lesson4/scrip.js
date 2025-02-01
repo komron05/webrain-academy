8715757afc8cca95a58a191451f7cf061421b6ff
Melikulov Komronbek <komronbekmelikulov0@gmail.com>
2025-02-01 15:07:06 +0800

    Switch | For loop

---
+++ lesson4/scrip.js
@@ -0,0 +1,80 @@
+// console.log(0 || 1);
+// console.log(1 ?? 1);
+//==============================================
+
+// if --> <, >, <=, >=, ==, ===, if xamma belgilarga teng kata kichik teng kata||teng kichi||teng 
+// switch --> === switch faqat teng 3-ta tenglikka
+
+// let data = "yomgir"
+// switch(data){
+//     case 'yomgir':console.log('bugun yomgir');
+// }
+
+//==============================================
+
+// let vremya = "isfiq"
+// switch(vremya ){
+//     case 'yomgir':console.log('bugun yomgir');
+//     break;
+//     case 'qor':console.log('bugun qor');
+//     break;
+//     case 'bulut':console.log('bugun bulut');
+//     break;
+//     case 'isiq':console.log('bugun isiq');
+//     break;
+//     default:
+//         console.log('no data');
+        
+// };
+
+//==============================================
+// for loops
+// // for (bashlangich, manzil, step);
+//  sum = 0;
+// for(i=1;i<=12;i++){
+//     console.log(`sam = ${sum} + ${i} = ${(sum +=i)} `);
+//   if(sum==10) break;
+// }
+//==============================================
+
+// for (i = 1; i < 12; i++ ){
+//     console.log(i);
+//     i = i * 2;
+// }
+
+//==============================================
+
+// for (i = 0; i < 12; i++ ){
+// if( i % 2==0 ){
+//     console.log(`${i} juft son`);
+// }else{
+//     console.log(`${i} toq son`);
+    
+// }
+// } 
+
+//==============================================
+
+// for (i = 1; i < 12; i++ ){
+//     if(i % 3 == 0) continue;
+//     console.log(i);
+    
+//     };
+    
+//==============================================
+// let s =10;
+//   for (s; s >= 0; s--){
+//     console. log(s);
+// };
+
+//==============================================
+// for(i=2; i <= 10;i++){
+//     for(j=2;j <= 10;j++){
+//         console.log(`${i} x ${j} = ${i*j}`);
+//     } 
+// };
+//==============================================
+
+
+
+//==============================================
