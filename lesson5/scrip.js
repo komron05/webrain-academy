e78da193144b5c7efbdbd4beda6aa3fe26d3a221
Melikulov Komronbek <komronbekmelikulov0@gmail.com>
2025-02-04 20:12:45 +0800

    While | Do While | Fanction

---
+++ lesson5/scrip.js
@@ -0,0 +1,116 @@
+//while null, undefined, 0, false, da while ishlamidi 
+// while (condition) {
+// }
+//===================================
+// let i = 3;
+// while (i) {
+//     console.log(i);
+//     i--;
+// }
+//===================================
+
+//Do while xam xuddi while uxshaydi 
+//keys jihattan DO birinchi konsolga chiqaradi kein while ishlaydi while dan farqi shu;
+// let j = 3;
+// do {
+//  console.log(j);
+//  j--; 
+// }while (j)
+    
+//===================================
+
+// let salo = document.querySelector('.bos')
+// document.querySelector('.bos').addEventListener('click',function(){
+//     alert('salom komronbek qandaysiz nima yordam bera olaman')
+// })
+//===================================
+// FANCTION - Declaration
+
+// gitAvg()
+// gitAvg()
+// function gitAvg() {
+// console.log('komron1');
+// console.log('komron2');
+// console.log('komron3');
+// console.log('komron4');
+// console.log('komron5');
+// console.log('komron6');
+// console.log('komron7');
+// console.log('================================');
+// }
+
+//===================================
+// FANCTION - expresstion 
+
+// const getAvr = function(){
+    //    console.log('Komronbek Welcom to javascript');
+    
+    // }
+    // getAvr()
+
+//===================================
+// FANCTION - Collback
+
+
+
+//===================================
+
+// FANCTION - Arrow
+//===================================
+//PARAMETIR
+// const getAvr = function(name,surname,age){
+//    console.log(`Name ${name}`);
+//    console.log(`Surname ${surname} `);
+//    console.log(`Age ${age} `);
+//    console.log(`====================================`);
+   
+// }
+// getAvr('Samardin','jahbarov',1111)
+// getAvr('Jurabek','rahimqulov',2222)
+// getAvr('Firdavs','mirzarahimov',3333)
+// getAvr('Dilshod','Ergashov',4444)
+// getAvr('Komronbek','Melikulov',5555)
+
+//===================================
+
+// const telegram = function(name,surname){
+//     if (surname) console.log(name, surname);
+//     else console.log(name);
+// }
+// telegram('komronbek')
+//===================================
+//DEFAULT PARAMETIR
+// const  enstagram = function(name,surname=""){
+    //     console.log(name,surname);
+    // }
+    // enstagram('komronbek','melikulov') //Manabu bugungu kunda ishlatiladigan versiyasi
+
+
+    // const enstagram = function(name,surname){
+    //     surname ? console.log(name,surname) : console.log(name);
+    // }
+    // enstagram('melikulov')
+    //===================================
+    //RETURN maqsad anashu Function qaytoyotgan natija ustuda qandaydur amal ishlatilish uchun ishlatiladi
+    // const tot = function(a){
+    // let totul = 193;
+    // return (a*100)/totul;
+    // }
+    // console.log(parseInt(tot(159)));
+
+    //===================================
+
+    // const gpe = function(a=0,b=0,c=0,d=0){
+    //     let res = (a + b + c + d)/4;
+    //     return res;
+    // }
+    // console.log(gpe(12,23,12,12));
+    // console.log(gpe(13,24,11,17));
+    // console.log(gpe(19,20,72,16));
+
+    //===================================
+    
+
+    //===================================
+    //===================================
+    //===================================
